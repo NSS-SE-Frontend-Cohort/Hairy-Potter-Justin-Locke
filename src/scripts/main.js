0b47d97bf115ba17ac6f9a9f8d9acd2f5d1049b0
Justin Locke <Justindeanlocke@gmail.com>
2024-12-30 13:49:43 -0600

    Added potteryWheel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 // Imports go first
-
+import { makePottery } from "./potteryWheel"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery(mug, 4, 10)
+console.log(mug)
+let ornament = makePottery(ornament, 2, 5)
+let platter = makePottery(platter, 7, 2)
+let bowl = makePottery(bowl, 12, 4)
 
 
 // Fire each piece of pottery in the kiln
---
+++ src/scripts/potteryWheel.js
@@ -0,0 +1,19 @@
+//variable to have the value of the primary key for each piece of pottery. Initial value of 1.
+let primaryKey = 1;
+//Define and export a function named makePottery.
+//Function must accept Shape, weight, height as parameters
+//function must return an object with shape, weight, height, id (increment each time the function is invoked) as properties.
+
+export const makePottery = (shape, weight, height) => {
+    
+    const potteryPiece = {
+        shape: shape,
+        weight: weight,
+        height: height,
+        id: primaryKey
+    }
+    
+    primaryKey += 1;
+
+    return potteryPiece;
+}
