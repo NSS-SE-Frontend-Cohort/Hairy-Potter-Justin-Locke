7dd8fc65ed67b9f06c6db3048c7bda0c2bdcf2f5
Justin Locke <Justindeanlocke@gmail.com>
2024-12-30 14:19:52 -0600

    Passed pottery create test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,12 @@
-// Imports go first
-import { makePottery } from "./potteryWheel"
+import { makePottery } from './potteryWheel.js'
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery(mug, 4, 10)
-console.log(mug)
-let ornament = makePottery(ornament, 2, 5)
-let platter = makePottery(platter, 7, 2)
-let bowl = makePottery(bowl, 12, 4)
-
+let mug = makePottery("Mug", 4, 10)
+let ornament = makePottery("Ornament", 2, 5)
+let platter = makePottery("Platter", 7, 2)
+let bowl = makePottery("Bowl", 12, 4)
+let planter = makePottery("Planter", 50, 25)
 
 // Fire each piece of pottery in the kiln
 
--- src/scripts/potteryWheel.js
+++ src/scripts/potteryWheel.js
@@ -11,7 +11,7 @@ export const makePottery = (shape, weight, height) => {
         weight: weight,
         height: height,
         id: primaryKey
-    }
+    };
     
     primaryKey += 1;
 
